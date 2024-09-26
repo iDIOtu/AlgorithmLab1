c5f384e1e404379b4b1a9653244e5fd16d021b75
iDIOtu <95068240+iDIOtu@users.noreply.github.com>
2024-09-26 19:38:15 +0500

    Добавьте файлы проекта.

---
+++ AlgorithmLab1/GetInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlgorithmLab1
+{
+    internal class GetInput
+    {
+        static Random random = new Random();
+        public static int[] VectorInput(int n)
+        {
+            int[] vector = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = random.Next();
+            }
+
+            return vector;
+        }
+    }
+}
