f8154c95b528e987da4a6a083ae940ee8ec24806
gippster <sajbullin77@gmail.com>
2024-09-30 17:17:21 +0500

    constalg

---
+++ AlgorithmLab1(console)/Algorithms/ConstAlg.cs
@@ -0,0 +1,17 @@
+﻿using AlgorithmLab1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmLab1_console_.Algorithms
+{
+	internal class ConstAlg : Algorithm
+    {
+		public override void ExecuteAlgorithm(int[] vector) 
+		{
+			return 1;
+		}
+	}
+}
